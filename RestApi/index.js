4cae536f4417e14204dec374f8a06e251294fcc1
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-10-28 14:36:01 +0530

    upadte Rest api

--- RestApi/index.js
+++ RestApi/index.js
@@ -4,10 +4,16 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 5000;
 
-// Routes //
-
-app.get("/users", (req, res) => {
+// Route all see all the users //
+app.get("/api/users", (req, res) => {
   return res.json(users);
+});
+
+// Route to get the user with id 1 //
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
 });
 
 app.listen(PORT, () => {
